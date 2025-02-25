4c6bf30dedd36df738ae090316db731c2f9a6de3
Alan <90423094+Alan-Tomaz@users.noreply.github.com>
2025-02-25 17:08:32 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyCjz55lFWA4XKw8-mxjv6DFeLtbFEk2aEc",
-    authDomain: "lupi-delivery-f3d66.firebaseapp.com",
-    projectId: "lupi-delivery-f3d66",
-    storageBucket: "lupi-delivery-f3d66.firebasestorage.app",
-    messagingSenderId: "146601309708",
-    appId: "1:146601309708:web:e81326768adabac12ba029",
-    measurementId: "G-6W60Q9TX14"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
